e49701c3f103fe0d3a5ed868923ac9e68cc5f43b
Ed Madrid <edmadrid@gmail.com>
2021-03-11 11:36:48 -0500

    modify footer content

--- footer.php
+++ footer.php
@@ -13,17 +13,8 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'culpodcasts' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'culpodcasts' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'culpodcasts' ), 'culpodcasts', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+      <h2>Contact us</h2>
+      <p>Contact the Digital Scholarship team via email at: <a href="mailto:publishing@library.columbia.edu">publishing@library.columbia.edu</a></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
